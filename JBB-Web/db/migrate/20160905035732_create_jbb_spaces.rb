6841c3dee787d0975c5affabfeb7043b1c1087cb
Diego <diegobmf@gmail.com>
2016-09-05 19:14:45 -0300

    Fix for the "id_jbb" error Returned the removed atribute id_jbb to 20160905035732_create_jbb_spaces.rb

--- JBB-Web/db/migrate/20160905035732_create_jbb_spaces.rb
+++ JBB-Web/db/migrate/20160905035732_create_jbb_spaces.rb
@@ -1,6 +1,7 @@
 class CreateJbbSpaces < ActiveRecord::Migration[5.0]
   def change
     create_table :jbb_spaces do |t|
+			t.integer :id_jbb
       t.string :space_name
       t.string :description
       t.timestamps
