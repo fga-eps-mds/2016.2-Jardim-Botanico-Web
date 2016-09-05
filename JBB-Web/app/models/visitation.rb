a79c98bca82444e0696437edbe878671562a84b4
Diego <diegobmf@gmail.com>
2016-09-05 01:40:12 -0300

    Add validations to Visitation's model Added validations to date, time, CPF, visitants amount and description attributes

--- JBB-Web/app/models/visitation.rb
+++ JBB-Web/app/models/visitation.rb
@@ -1,2 +1,17 @@
 class Visitation < ApplicationRecord
+	
+	# validation of date
+	validates :date, presence: true
+	
+	# validation of CPF
+	validates :cpf, presence: true
+	
+	# validation of time
+	validates :time, presence: true
+	
+	# validation of visitants amount
+	validates :visitants_amount, presence: true
+	
+	# validation of description
+	validates :description, presence: true, length: { minimum: 5, maximum: 300 }
 end
