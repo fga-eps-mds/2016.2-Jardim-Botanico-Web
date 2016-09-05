6afb4ef51a7834cfb26d8e37fa203d8aacd2280e
isaque <alvesisaque838.ia@gmail.com>
2016-09-05 05:15:35 +0000

    Add validations to Event's model Adding validations of presence and lenght

--- JBB-Web/app/models/event.rb
+++ JBB-Web/app/models/event.rb
@@ -1,19 +1,21 @@
 class Event < ApplicationRecord
 
-#validating name of trail
-#required field, maximum size 50 characters , minimum size 3 characters
-validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+	# validation of date_time
+	validates :date_time, presence: true
 
-#validating length
-#required field, number positive
-#validates :length, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+	# validation of name
+	validates :name, presence: true	
 
-#validating locomotion
-#required field, maximum size 10 characters
-validates :length, presence: true, length: { maximum: 10 }
+	# is_confirmed
+	#validates :is_confirmed, presence: true
 
-#validating description of trail
-#required field, maximum size 300 characters , minimum size 5 characters
-validates :description, presence: true, length: { minimum: 5, maximum: 300 }
+	# validation of description
+	validates :description, presence: true, length: { maximum: 300}
+
+	# validation of people_amount
+	validates :is_confirmed, presence: true
+
+	# validation of cpf
+	validates :is_confirmed, presence: true
 
 end
