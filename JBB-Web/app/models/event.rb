d8e3001099f230df7385a0c7c03a61dd5dc31f91
isaque <alvesisaque838.ia@gmail.com>
2016-09-05 05:15:35 +0000

    Add validations to Event's model Adding validations of presence and lenght

--- JBB-Web/app/models/event.rb
+++ JBB-Web/app/models/event.rb
@@ -1,3 +1,23 @@
 class Event < ApplicationRecord
+
 	belongs_to :user
+
+	# validation of date_time
+	validates :date_time, presence: true
+
+	# validation of name
+	validates :name, presence: true	
+
+	# is_confirmed
+	#validates :is_confirmed, presence: true
+
+	# validation of description
+	validates :description, presence: true, length: { maximum: 300}
+
+	# validation of people_amount
+	validates :is_confirmed, presence: true
+
+	# validation of cpf
+	validates :is_confirmed, presence: true
+
 end
