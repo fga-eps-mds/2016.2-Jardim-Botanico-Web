5a27215aa2553f2c3ee26f902e3eb3a1dcefc0a2
Bernardohrl <bernardohrl.trabalho@gmail.com>
2016-09-06 20:37:52 -0300

    Reworking on Employee Validation Doing validations with methods

--- JBB-Web/app/models/employee.rb
+++ JBB-Web/app/models/employee.rb
@@ -1,14 +1,29 @@
 class Employee < ApplicationRecord
 
   #name
+  special_caracter = {'!', '@', '#', '$', '%', '¨', '*', '(', ')', '-', '+', '=', '§', '_',
+  					  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '²', '¹', '³', '¢',
+  					   '¬', '{', '[', ']', '}' '?', ':', ';', '.', ',', '°', 'º', '|', '\\'
+  					   '/', '^', '~', '´', '`', '"', '\'', '<', '>'}
+
+  def validate_name (name)
+  	if name.bytesize < 3
+  		return false
+  	elsif
+  		special_caracter.each do |x|
+
+
   validates :employee_name, presence:true
+  
 
   #email
-  #validates  :cpf  usar_como_cpf
-  usar_como_cpf :cpf
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { minimum:10 , maximum: 255 },
+  format: { with: VALID_EMAIL_REGEX }
+  
 
   #birth
-
 
 
   #phone
