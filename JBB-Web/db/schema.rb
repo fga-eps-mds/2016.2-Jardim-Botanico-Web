c7951839b5badd5042c6b6fa9ae54118d6ca9833
Mariana Mendes <mare.s.mendes@gmail.com>
2016-09-06 17:34:13 +0000

    Correcting some mistakes Correcting the view/application

--- JBB-Web/db/schema.rb
+++ JBB-Web/db/schema.rb
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 20160917142511) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "trails", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.float    "length",      limit: 24
+    t.string   "locomotion"
+    t.string   "description"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email"
     t.string   "name"
