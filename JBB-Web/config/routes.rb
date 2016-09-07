38d65e4a749fb2fb84dd0cc0ab32bf80494f1872
marimendes <mare.s.mendes@gmail.com>
2016-09-06 21:11:40 -0300

    Remove files with devise gem.

--- JBB-Web/config/routes.rb
+++ JBB-Web/config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
 
   get 'employees/new'
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- JBB-Web/test/controllers/employees_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EmployeesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get employees_new_url
-    assert_response :success
-  end
-
-end
