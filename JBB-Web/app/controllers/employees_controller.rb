860363f75dc9b399c86e38ca02c6c21f2af5ef6c
isaque <alvesisaque838.ia@gmail.com>
2016-09-08 03:18:25 +0000

    Creating employees controller shares

--- JBB-Web/app/controllers/employees_controller.rb
+++ JBB-Web/app/controllers/employees_controller.rb
@@ -1,4 +1,25 @@
 class EmployeesController < ApplicationController
   def new
+  	@employee = Employee.new
   end
+
+  def index
+  	@employees = Employee.all
+  end
+
+  def create
+    @employee = CadastroMenor.new(employee_params)
+
+    respond_to do |format|
+      if @employee.save
+        format.html { redirect_to @employee, notice: 'Cadastro foi criado com sucesso.' }
+        format.json { render :show, status: :created, location: @employee }
+      else
+        format.html { render :new }
+        format.json { render json: @employee.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def 
 end
