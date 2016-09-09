ace41ed3b2f9f281ff368ee643f1a045039a93cc
Mariana Mendes <mare.s.mendes@gmail.com>
2016-09-09 02:02:00 +0000

    Setting user's controller methods

--- JBB-Web/app/controllers/users_controller.rb
+++ JBB-Web/app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		log_in @user
+  		flash[:sucess] = "Bem vindo ao Jardim Botânico"
+  		redirect_to @user
+  	else
+  		render 'new'
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation, :cpf, :gender, :phone, :birth)
+  	end
+
+
 end
