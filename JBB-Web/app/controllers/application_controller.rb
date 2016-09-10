8ae2ab940e934b3096cefb9ce7f1d354a63ec2db
isaque <alvesisaque838.ia@gmail.com>
2016-09-10 23:30:06 +0000

    Modeling visitation Working on visitation controller

--- JBB-Web/app/controllers/application_controller.rb
+++ JBB-Web/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+private
+
+def current_visitation
+	@current_visitation ||= Visitation.find(session[:visitation_id])  if session[:visitation_id]
+end
+
 end
--- JBB-Web/app/controllers/visitations_controller.rb
+++ JBB-Web/app/controllers/visitations_controller.rb
@@ -1,4 +1,24 @@
 class VisitationsController < ApplicationController
   def new
+  	@visitation = Visitation.new
   end
+
+
+  def create
+  	@visitation = Visitation.new(visitation_params)
+  		if @visitation.save 
+  			redirect_to @visitation, notice: "Evento criado"
+  		else
+  			render action: :new
+  		end
+  end
+
+
+
+
+private
+def visitation_params
+    params.require(:visitation).permit(:date, :cpf, :time, :isConfirmed, :visitants_amount, :description)
+end
+
 end
