2adfcb3a5c1774837e2f4a0967dc573691436472
Mariana Mendes <mare.s.mendes@gmail.com>
2016-09-12 22:56:40 +0000

    Starting the welcome page

--- JBB-Web/app/models/user.rb
+++ JBB-Web/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 has_secure_password
 validates :password_digest, presence: true, length: {minimum: 6}
 
+
   #default_values
   def set_default
     self.name = 'no_name'
