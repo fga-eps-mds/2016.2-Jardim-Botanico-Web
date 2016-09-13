79c45fe4a8a9b39ef8397a276fe80ce628f67928
Mariana Mendes <mare.s.mendes@gmail.com>
2016-09-13 01:29:44 +0000

    Create the user's show page

--- JBB-Web/app/controllers/users_controller.rb
+++ JBB-Web/app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
   def new
-    if !logged_in?
-  	 @user = User.new
-    end
-    redirect_to home_path
+  	@user = User.new
   end
 
   def create
   	@user = User.new(user_params)
   	if @user.save
-      redirect_to @user, notice: "Usuário foi criado com sucesso!"	
+      redirect_to @user, notice: "Cadastro efetuado com sucesso!"	
   	else
   		render 'new'
     end
