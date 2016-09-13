16930ef3d408b232cf91a7af2b345f63ca1e09fc
isaque <alvesisaque838.ia@gmail.com>
2016-09-13 22:14:59 +0000

    Editing validation CPF Correcting errors in the user cpf validation

--- JBB-Web/db/schema.rb
+++ JBB-Web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913184454) do
+ActiveRecord::Schema.define(version: 20160913193739) do
 
   create_table "employees", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "cpf"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20160913184454) do
     t.string   "name"
     t.string   "description"
     t.integer  "peaple_amount"
-    t.integer  "cpf"
+    t.string   "cpf"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "status"
