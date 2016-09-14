ea8abfd4841e5d4597514b877ca153e7ea39501e
isaque <alvesisaque838.ia@gmail.com>
2016-09-14 23:53:28 +0000

    Editing controller Adding methods of events_controller.rb and editing application_controller.rb

--- JBB-Web/app/controllers/application_controller.rb
+++ JBB-Web/app/controllers/application_controller.rb
@@ -1,19 +1,25 @@
 class ApplicationController < ActionController::Base
 
-
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
     include SessionsHelper
+
   end
 
 
 private
 
-def current_visitation
-	@current_visitation ||= Visitation.find(session[:visitation_id])  if session[:visitation_id]
-end
+	def current_visitation
+		@current_visitation ||= Visitation.find(session[:visitation_id])  
+		if session[:visitation_id]
+		end
+	end
 
+	def current_event
+		@current_event ||= Event.find(session[:event_id])  
+		if session[:event_id]
+		end
+	end
 end
-
 
--- JBB-Web/app/controllers/events_controller.rb
+++ JBB-Web/app/controllers/events_controller.rb
@@ -1,4 +1,24 @@
 class EventsController < ApplicationController
   def new
+  	@event = Event.new
   end
+#########################################################################################################
+  def create
+  	@event = Event.new(event_params)
+  	if @event.save 
+  		redirect_to @event, notice: "Evento criado"
+  	else
+  		render action: :new
+  	end
+  end
+#########################################################################################################
+  def index
+  	@event = Event.all
+	end
+#########################################################################################################
+	private
+  def event_params
+    params.require(:event).permit(:date, :time, :status, :description, :people_amount)
+  end
+#########################################################################################################
 end
