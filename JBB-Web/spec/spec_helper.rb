c38535ca820978295a3a77ea44c07d3907cf0d44
nicacioneto <nicacionetobsb@hotmail.com>
2016-09-15 22:10:04 +0000

    SimpleCov Adding SimpleCov: Code coverage analysis tool for Ruby that works with rspec.

--- JBB-Web/spec/spec_helper.rb
+++ JBB-Web/spec/spec_helper.rb
@@ -16,6 +16,14 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start do
+  SimpleCov.maximum_coverage_drop 5
+  SimpleCov.minimum_coverage 90
+end
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
