f839345365aacf23ec1a96ea738882f5c382999a
Isaque Alves <alvesisaque838.ia@gmail.com>
2016-09-14 21:30:55 -0300

    Remove migrations from db folder

--- JBB-Web/db/migrate/20160913184454_create_status_column_in_events_and_visitations.rb
+++
@@ -1,6 +0,0 @@
-class CreateStatusColumnInEventsAndVisitations < ActiveRecord::Migration[5.0]
-  def change
-		add_column :visitations, :status, :string
-		add_column :events, :status, :string
-  end
-end
--- JBB-Web/db/migrate/20160913193739_change_cpf_type_to_string_in_events_table.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCpfTypeToStringInEventsTable < ActiveRecord::Migration[5.0]
-  def change
-  	change_column :events, :cpf, :string
-  end
-end
