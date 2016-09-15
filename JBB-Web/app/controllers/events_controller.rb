895cbabe7c47889a969e7f6b6233eaea52046d6f
Diego <diegobmf@gmail.com>
2016-09-14 23:30:24 -0300

    Add methods in events controller Added methods to filtrate the events in event's table based in "Aguardando confirmacao" e "Confirmado" status

--- JBB-Web/app/controllers/events_controller.rb
+++ JBB-Web/app/controllers/events_controller.rb
@@ -1,24 +1,40 @@
 class EventsController < ApplicationController
-  def new
-  	@event = Event.new
-  end
-#########################################################################################################
-  def create
-  	@event = Event.new(event_params)
-  	if @event.save 
-  		redirect_to @event, notice: "Evento criado"
-  	else
-  		render action: :new
-  	end
-  end
-#########################################################################################################
-  def index
-  	@event = Event.all
+	def new
+		@event = Event.new
 	end
-#########################################################################################################
+
+
+	def create
+		@event = Event.new(event_params)
+		if @event.save 
+			redirect_to @event, notice: "Evento criado"
+		else
+			render action: :new
+		end
+	end
+
+
+	def index
+		@event = Event.all
+	end
+
+	# Metodo para filtrar os eventos de acordo com o status "Aguardando confirmacao"
+	def confirmations_request
+		#@event = Events.all
+		@event = @event.where(status: "Aguardando confirmacao")
+	end
+
+
+	# Metodo para filtrar os eventos de acordo com o status "Confirmado"
+	def cancel_event
+		#@event = Events.all
+		@event = @event.where(status: "Confirmado")
+	end
+
+
 	private
-  def event_params
-    params.require(:event).permit(:date, :time, :status, :description, :people_amount)
-  end
-#########################################################################################################
+	def event_params
+		params.require(:event).permit(:date, :time, :status, :description, :people_amount)
+	end
+
 end
