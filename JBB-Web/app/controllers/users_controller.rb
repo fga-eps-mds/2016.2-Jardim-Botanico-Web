296e8543d00911db03144b9d9853dbc57261c7d0
egewarth <egewarth@gmail.com>
2016-09-15 00:10:01 +0000

    C1C2UC16 - Manter Usuario Editing form for editing and creating User

--- JBB-Web/app/controllers/users_controller.rb
+++ JBB-Web/app/controllers/users_controller.rb
@@ -16,14 +16,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    redirect_to home_path
+  end
+
   def show
     @user = User.find(params[:id])
     if @user != current_user
       redirect_to home_path
     end
-  end
-
-  def update
   end
 
   def delete
