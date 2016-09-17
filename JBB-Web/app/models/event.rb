392dccfcb4831e6a6b4224cb8e2b4f5898c47592
Isaque Alves <alvesisaque838.ia@gmail.com>
2016-09-17 18:54:27 -0300

    Creating message status for Events

--- JBB-Web/app/models/event.rb
+++ JBB-Web/app/models/event.rb
@@ -11,6 +11,26 @@ class Event < ApplicationRecord
 
 	# status
 	#validate :set_status_as_default
+	#status
+	def set_status_default
+		self.status = "Aguardando confirmacao"
+	end	
+
+	def canceled_by_employee
+		self.status = "Cancelado por funcionario"
+	end
+
+	def canceled_by_user
+		self.status = "Cancelado pelo usuario"
+	end
+
+	def refused_by_employee
+		self.status = "Recusado por funcionario"
+	end
+
+	def accepted_by_employee
+		self.status = "Agendado"
+	end
 
 	# validation of description
 	validates :description, presence: true, length: { minimum: 5, maximum: 300 }
