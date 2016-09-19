e6b4c9dab7931abaf6c17ca6f932e72a4afd3828
egewarth <egewarth@gmail.com>
2016-09-19 04:02:00 +0000

    Frontend Fixing up error of formatting masks

--- JBB-Web/app/assets/javascripts/masks.js
+++ JBB-Web/app/assets/javascripts/masks.js
@@ -46,6 +46,7 @@ function mdate(v){
     hifen = '-'
     if($.isNumeric(v) || hifen.localeCompare(v)) {
         v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
+        v=v.replace(/^(\d{2})(\d)/g,"$1/$2");
         v=v.replace(/^(\d{2})(\d)/g,"$1/$2"); //Coloca parênteses em volta dos dois primeiros dígitos
         v=v.replace(/(\d)(\d{4})$/,"$1/$2");    //Coloca hífen entre o quarto e o quinto dígitos
         return v;
