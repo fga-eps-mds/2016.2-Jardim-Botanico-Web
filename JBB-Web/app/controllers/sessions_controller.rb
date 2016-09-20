1502ec309bb313af72c630b9a31fc0508035fe96
bernardohrl <bernardohrl.trabalho@gmail.com>
2016-09-20 19:57:46 +0000

    Fixing user create Now user's create is  working

--- JBB-Web/app/controllers/sessions_controller.rb
+++ JBB-Web/app/controllers/sessions_controller.rb
@@ -1,16 +1,25 @@
 class SessionsController < ApplicationController
   before_action :block_access, except: [:destroy]
+  
+  def new
+    render 'new'
+  end
+
   def create
     @user = User.find_by(email: params[:session][:email])
+    
     if @user && @user.authenticate(params[:session][:password_digest])
+      session[:user_id] = @user.id
       sign_in(@user)
-    redirect_to @user
+      redirect_to home_path
     else
       render 'new'
     end
   end
+  
   def destroy
     sign_out
     redirect_to root_url
   end
+
 end
