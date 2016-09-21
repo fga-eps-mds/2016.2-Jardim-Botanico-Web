c9a31abac9e653337f53269ac96b621d257dd5c0
bernardohrl <bernardohrl.trabalho@gmail.com>
2016-09-21 01:42:16 +0000

    Fixing user cpf Now user's cpf is validating

--- JBB-Web/app/models/user.rb
+++ JBB-Web/app/models/user.rb
@@ -19,7 +19,7 @@ validates :password_digest, presence: true, length: {minimum: 6}
     self.is_employee = false
   end
 
-  validate :valid_name, :valid_birth, :valid_gender, :valid_email, :valid_cpf #:valid_phone,
+  validate :valid_name, :valid_birth, :valid_gender, :valid_email, :valid_cpf
 
   $SPECIAL_CARACTERS = ['!', '@', '#', '$', '%', '¨', '*', '(', ')', '-', '+', '=', '§', '_',
     '²', '¹', '³', '¢', '¬', '{', '[', ']', '}' '?', ':', ';', '.', ',',
@@ -65,27 +65,99 @@ validates :password_digest, presence: true, length: {minimum: 6}
   end
 
 
-  def valid_cpf(cpf=nil)
-    return false if cpf.nil?
 
-    invalid_cpf = %w{12345678909 11111111111 22222222222 33333333333 44444444444 55555555555 66666666666 77777777777 88888888888 99999999999 00000000000}
-    cpf_numbers = cpf.scan /[0-9]/
-    if cpf_numbers.length == 11
-      unless invalid_cpf.member?(cpf_numbers.join)
-        cpf_numbers = cpf_numbers.collect{|x| x.to_i}
-        sum = 10*cpf_numbers[0]+9*cpf_numbers[1]+8*cpf_numbers[2]+7*cpf_numbers[3]+6*cpf_numbers[4]+5*cpf_numbers[5]+4*cpf_numbers[6]+3*cpf_numbers[7]+2*cpf_numbers[8]
-        sum = sum - (11 * (sum/11))
-        wresult1 = (sum == 0 or sum == 1) ? 0 : 11 - sum
-        if wresult1 == cpf_numbers[9]
-          sum = cpf_numbers[0]*11+cpf_numbers[1]*10+cpf_numbers[2]*9+cpf_numbers[3]*8+cpf_numbers[4]*7+cpf_numbers[5]*6+cpf_numbers[6]*5+cpf_numbers[7]*4+cpf_numbers[8]*3+cpf_numbers[9]*2
-          sum = sum - (11 * (sum/11))
-          wresult2 = (sum == 0 or sum == 1) ? 0 : 11 - sum
-          return true if wresult2 == cpf_numbers[10] # CPF validado
-        end
+
+  #cpf
+  def valid_cpf
+
+    int_cpf = [1,2,3,4,5,6,7,8,9,0,0]
+    count1 = 10
+    count2 = 8
+    digit = 0
+
+
+    invalid_cpf = ['123.456.789-09', '111.111.111-11', '222.222.222-22', '333.333.333-33', '444.444.444-44',
+         '555.555.555-55', '666.666.666-66', '777.777.777-77', '888.888.888-88', '999.999.999-99',
+         '000.000.000-00']
+
+
+    if self.cpf == nil
+      errors.add(:cpf, "Employee's cpf is null.")
+      return
+    end
+
+    invalid_cpf.each do |invalid|
+      if (invalid <=> self.cpf) == 0
+        errors.add(:cpf, "Invalid sequerence of numbers.")
       end
     end
-    return false
+
+    cpf_splited = self.cpf.split(//)
+
+=begin
+    for count1 in 0..13
+        unless cpf_splited[count1] == "." || cpf_splited[count1] == "-"
+          cpf_int[digit] = cpf_splited[count1].to_i
+          digit = digit + 1
+        end
+    end
+=end
+
+    for count1 in 0..13
+        unless cpf_splited[count1] == "." || cpf_splited[count1] == "-"
+            int_cpf[digit] = cpf_splited[count1].to_i
+            digit = digit + 1
+        end
+    end
+
+    count1 = 0
+    digit = 0
+
+    puts int_cpf
+    
+    #calculating first digit
+    for count1 in 2..10
+      digit += int_cpf[count2]*count1
+      count2 = count2-1
+    end
+
+    puts digit
+
+    digit = ((digit*10)%11)
+    
+    if digit == 10
+      digit = 0
+    end
+
+    #validating first digit
+    unless digit == int_cpf[9]
+      errors.add(:cpf, "Invalid CPF.")
+      return
+    end
+
+    #calculating second digit
+    count2 = 9
+    for count1 in 2..11
+      digit += int_cpf[count2]*count1
+      count2 = count2-1
+    end
+
+    digit = ((digit*10)%11)
+    if digit == 10
+      digit = 0
+    end
+
+    #validating second digit
+    unless digit == int_cpf[10]
+      errors.add(:cpf, "Invalid CPF.")
+      return
+    end
+  
+    return true
   end
+
+
+
 
 
   #email
