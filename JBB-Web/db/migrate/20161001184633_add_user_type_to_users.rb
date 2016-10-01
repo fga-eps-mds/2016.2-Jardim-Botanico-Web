5fce3e98ebb533be0171b318c70a83a5c5684416
Mariana Mendes <mare.s.mendes@gmail.com>
2016-10-01 18:53:40 +0000

    User Migration Add user_type on Users
    Signed-off-by: Isaque Alves  <alvesisaque838.ia@gmail.com>

---
+++ JBB-Web/db/migrate/20161001184633_add_user_type_to_users.rb
@@ -0,0 +1,5 @@
+class AddUserTypeToUsers < ActiveRecord::Migration[5.0]
+  def change
+  	add_column :users, :user_type, :string
+  end
+end
--- JBB-Web/db/schema.rb
+++ JBB-Web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917142511) do
+ActiveRecord::Schema.define(version: 20161001184633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160917142511) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "user_type"
     t.index ["cpf"], name: "index_users_on_cpf", using: :btree
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["name"], name: "index_users_on_name", using: :btree
