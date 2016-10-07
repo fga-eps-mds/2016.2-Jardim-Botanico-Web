b16f5d73ae02461ca14a984cce903d9fd4af5056
Bernardo Henrique <bernardohrl.trabalho@gmail.com>
2016-10-07 00:52:10 +0000

    Status change email ready Sends email, but not tested yet
    Signed-off-by: Mariana Mendes <mare.s.mendes@gmail.com>

--- JBB-Web/app/mailers/user_mailer.rb
+++ JBB-Web/app/mailers/user_mailer.rb
@@ -6,4 +6,12 @@ class UserMailer < ApplicationMailer
     @url  = 'http://localhost:3000/sign_in'
     mail(to: @user.email, subject: "Bem Vindo ao Jardim Botânico de Brasília")
   end
+
+  def change_status(visitation)
+    @user = User.find(visitation.user_id)
+    @visitation = visitation
+    @url  = 'http://localhost:3000/visitations/show'
+  	mail(to: @user.email, subject: "Seu status de agendamento foi alterado")
+  end
+
 end
