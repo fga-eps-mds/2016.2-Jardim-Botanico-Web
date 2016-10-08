0ab96dc7d4d6dbaddfc6fab6166c4bd2dcdff2d4
Nicacio neto <nicacionetobsb@hotmail.com>
2016-10-08 21:11:43 +0000

    Add tests and finishing user historie Add tests of visitation status

--- JBB-Web/db/schema.rb
+++ JBB-Web/db/schema.rb
@@ -1,4 +1,3 @@
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20161110221428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
   create_table "events", force: :cascade do |t|
     t.integer  "user_id"
     t.date     "date_start"
