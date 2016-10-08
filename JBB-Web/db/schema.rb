d53fb1954d1939a8fe0c1fbc048e98a7431ef3f5
João Henrique Egewarth <egewarth@gmail.com>
2016-10-08 19:05:23 +0000

    Fixing some unit tests Fixing user tests and trails test Signed-off-by: João Henrique Egewarth <egewarth@gmail.com> Signed-off-by: Nicacio Neto <nicacionetobsb@hotmail.com>

--- JBB-Web/db/schema.rb
+++ JBB-Web/db/schema.rb
@@ -1,3 +1,4 @@
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008010813) do
+ActiveRecord::Schema.define(version: 20161008181353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +48,15 @@ ActiveRecord::Schema.define(version: 20161008010813) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_phones_on_user_id", using: :btree
+  end
+
+  create_table "trails", force: :cascade do |t|
+    t.string   "trail_name"
+    t.float    "length"
+    t.string   "locomotion"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
