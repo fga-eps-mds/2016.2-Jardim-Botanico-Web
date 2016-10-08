58b3b27cd33908380765e026d5127695534287d6
Nicacio neto <nicacionetobsb@hotmail.com>
2016-10-08 21:11:43 +0000

    Add tests and finishing user historie Add tests of visitation status

---
+++ JBB-Web/spec/models/user_model_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  before do
+    @user = FactoryGirl.create(:user)
+  end
+
+  it { expect(@user).to respond_to(:email, :name, :cpf, :is_employee,:gender,:birth,:password,:password_digest) }
+
+  it { expect(@user).to be_valid }
+
+  describe "attribute" do
+
+    subject {FactoryGirl.attributes_for(:user) }
+
+    describe "email" do
+      it "must be given" do
+        subject["email"] = "meuemail"
+        expect(User.new(subject)).not_to be_valid
+      end
+    end
+
+    describe "name" do
+      it "must be given" do
+        subject["name"] = ""
+        expect(User.new(subject)).not_to be_valid
+        subject["name"] = "+-/*&%$#@}|:;"
+        expect(User.new(subject)).not_to be_valid
+        subject["name"] = "123456"
+        expect(User.new(subject)).not_to be_valid
+        subject["name"] = "ab"
+        expect(User.new(subject)).not_to be_valid
+
+      end
+    end
+
+     describe "gender" do
+       it "must be given" do
+         subject["gender"] = 'nil'
+         expect(User.new(subject)).not_to be_valid
+       end
+     end
+
+    # describe "birth" do
+    #   it "must be given" do
+    #     subject["birth"] = ""
+    #     expect(User.new(subject)).not_to be_valid
+    #   end
+    # end
+
+  end
+end
