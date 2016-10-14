5a0f28a8e699157df732ba8061c71654b941ecbc
Bernardohrl <bernardohrl.trabalho@gmail.com>
2016-10-14 15:37:55 +0000

    Fixing error Now, natural libary is working

---
+++ JBB-Web/app/assets/javascripts/map/markers/natural_library.js
@@ -0,0 +1,40 @@
+function addNaturalLibraryMarker(){
+    //set tea hoBiblioteca da Naturezao, will be used at infowindow
+    var naturalLibraryInfowindowContentString =
+        '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Biblioteca da Natureza</h1>'+
+        '<div id="bodyContent">'+
+        '<p>O <b>Biblioteca da Natureza</b>, Possui acervo literário infanto-juvenil, ' +
+        'livros didáticos, gibis e brinquedos com temática ambiental que '+
+        'estimulam a imaginação. Há ainda uma Sala Verde para exposição '+
+        'de vídeos ambientais e um espaço para realização de oficinas.'+
+        '<p>Leia mais em:  <a href="/jbb_spaces/narural_library">'+
+        'Biblioteca da Natureza</a> '+
+        '</div>'+
+        '</div>';
+
+    //set the infowindow with japanese garden info
+    var infowindow = new google.maps.InfoWindow({
+        content: naturalLibraryInfowindowContentString
+    });
+
+    //set the position in latitude and longitude of the marker
+    var naturalLibraryLatLng = {lat: -15.875422, lng: -47.836901};
+
+    //add the marker at the map in the position previously defined
+    var naturalLibraryMarker = new google.maps.Marker({
+        position: naturalLibraryLatLng,
+        map: map,
+        title: 'Biblioteca da Natureza'
+    });
+
+    //add a listener to verify if the marker is clicked
+    naturalLibraryMarker.addListener('click', function() {
+        infowindow.open(map, naturalLibraryMarker);
+    });
+
+    //set the marker on map
+    naturalLibraryMarker.setMap(map);
+}
