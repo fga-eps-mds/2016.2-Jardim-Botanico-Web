36d77a111e727063f61066dcade708cc5b832d91
Bernardohrl <bernardohrl.trabalho@gmail.com>
2016-10-20 00:28:51 -0200

    Create the "Ecológica" trail in the map
    Signed-off-by: Diego França <diegobmf@gmail.com>
    Signed-off-by: Bernardohrl <bernardohrl.trabalho@gmail.com>

--- JBB-Web/app/assets/javascripts/map/trails/Trails.js
+++ JBB-Web/app/assets/javascripts/map/trails/Trails.js
@@ -15,6 +15,13 @@ class Trails {
         strokeWeight: 3
     });
 
+    this.ecologicTrailPolyline = new google.maps.Polyline({
+        path: ecologicTrailCoords,
+        geodesic: true,
+        strokeColor: '#15317e',
+        strokeOpacity: 1,
+        strokeWeight: 3
+    });
 
 
 
@@ -28,12 +35,21 @@ class Trails {
       infowindow.open(map, this.matterTrailPolyline);
     });
 
+    this.ecologicTrailPolyline.addListener('click', function() {
+      infowindow.setContent(ecologicTrailContentString);
+      infowindow.setPosition({lat: -15.875535627259856, lng: -47.83130550387796});
+      infowindow.open(map, this.ecologicTrailPolyline);
+    });
+
   }
 
 
   setMap() {
     //Set Trails at the map
     this.matterTrailPolyline.setMap(map);
+
+    //Set Trails at the map
+    this.ecologicTrailPolyline.setMap(map);
 
     //Set markers as on
     this.trails_on = true;
@@ -43,6 +59,9 @@ class Trails {
   setNull() {
     //Set Trails as null
     this.matterTrailPolyline.setMap(null);
+
+    //Set Trails as null
+    this.ecologicTrailPolyline.setMap(null);
 
     //Set markers and infowindow as off
     this.trails_on = false;
