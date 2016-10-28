895b6546493c6e6a4eae6cbde445f8b059cbaea5
Diego <diegobmf@gmail.com>
2016-10-28 00:46:00 -0200

    Get language change Its a simple change, but now the program can get what languague is been used
    Signed-off-by: Bernardo Henrique <bernardohrl.trabalho@gmail.com>

--- JBB-Web/app/controllers/application_controller.rb
+++ JBB-Web/app/controllers/application_controller.rb
@@ -21,5 +21,6 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+    @language = I18n.locale
   end
 end
