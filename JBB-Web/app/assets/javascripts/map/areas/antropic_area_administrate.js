2af90bf8352427767bca65836a407e84c657e2e5
marimendes <mare.s.mendes@gmail.com>
2016-11-02 21:05:49 +0000

    Refactoring creation of administrate anthropic area Breaking vegetation creating function into two functions
    Signed-off-by: Mariana Mendes <mare.s.mendes@gmail.com>
    Signed-off-by: João Egewarth <egewarth@gmail.com>

--- JBB-Web/app/assets/javascripts/map/areas/antropic_area_administrate.js
+++ JBB-Web/app/assets/javascripts/map/areas/antropic_area_administrate.js
@@ -1,33 +1,29 @@
-var anthropicAreaAdministratePolygon;
-function addAnthropicAreaAdministratePolygon(){
-  var anthropicAreaAdministratePolygonCoords = [
-  {lat: -15.869482047803452, lng: -47.835094928741455},
-  {lat: -15.869585247066473, lng: -47.835631370544434},
-  {lat: -15.869667806438837, lng: -47.836060523986816},
-  {lat: -15.869771005606735, lng: -47.8364360332489},
-  {lat: -15.869894844538468, lng: -47.836865186691284},
-  {lat: -15.869946444070884, lng: -47.837069034576416},
-  {lat: -15.870348919970423, lng: -47.83695101737976},
-  {lat: -15.871618261773216, lng: -47.836575508117676},
-  {lat: -15.87138090578367, lng: -47.83574938774109},
-  {lat: -15.871019711350165, lng: -47.835856676101685},
-  {lat: -15.870596597047564, lng: -47.83613562583923},
-  {lat: -15.870452118789554, lng: -47.83578157424927},
-  {lat: -15.870276680765596, lng: -47.83527731895447},
-  {lat: -15.870183801749903, lng: -47.83491253852844},
-  {lat: -15.86991548435302, lng: -47.83495545387268},
-  {lat: -15.869482047803452, lng: -47.835084199905396},
-  ];
+var anthropicAreaAdministrate;
 
-  // Construct the polygon.
-  anthropicAreaAdministratePolygon = new google.maps.Polygon({
-    paths: anthropicAreaAdministratePolygonCoords,
-    strokeColor: '#589C5F',
-    strokeOpacity: 0.1,
-    strokeWeight: 3,
-    fillColor: '#589C5F',
-    fillOpacity: 0.50
-  });
+var anthropicAreaAdministrateCoordinates = [
+    {lat: -15.869482047803452, lng: -47.835094928741455},
+    {lat: -15.869585247066473, lng: -47.835631370544434},
+    {lat: -15.869667806438837, lng: -47.836060523986816},
+    {lat: -15.869771005606735, lng: -47.8364360332489},
+    {lat: -15.869894844538468, lng: -47.836865186691284},
+    {lat: -15.869946444070884, lng: -47.837069034576416},
+    {lat: -15.870348919970423, lng: -47.83695101737976},
+    {lat: -15.871618261773216, lng: -47.836575508117676},
+    {lat: -15.87138090578367, lng: -47.83574938774109},
+    {lat: -15.871019711350165, lng: -47.835856676101685},
+    {lat: -15.870596597047564, lng: -47.83613562583923},
+    {lat: -15.870452118789554, lng: -47.83578157424927},
+    {lat: -15.870276680765596, lng: -47.83527731895447},
+    {lat: -15.870183801749903, lng: -47.83491253852844},
+    {lat: -15.86991548435302, lng: -47.83495545387268},
+    {lat: -15.869482047803452, lng: -47.835084199905396},
+];
 
-  anthropicAreaAdministratePolygon.setMap(map);
+var anthropicAreaAdministrateColor = '#589C5F';
+
+function addAnthropicAreaAdministrate(){
+  anthropicAreaAdministrate = createVegetation(anthropicAreaAdministrateCoordinates,
+                                        anthropicAreaAdministrateColor);
+
+  anthropicAreaAdministrate.setMap(map);
 }
