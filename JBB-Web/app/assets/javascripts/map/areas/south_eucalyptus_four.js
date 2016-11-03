7fa49921f8cb38c43d8a20792df8cbadea7f3c27
João Henrique Egewarth <egewarth@gmail.com>
2016-11-03 12:27:52 +0000

    Refactoring creation of vegetations Breaking creation function of vegetation in two functions on South eucalyptus four
    Signed-off-by: Mariana Mendes <mare.s.mendes@gmail.com>
    Signed-off-by: João Egewarth <egewarth@gmail.com>

--- JBB-Web/app/assets/javascripts/map/areas/south_eucalyptus_four.js
+++ JBB-Web/app/assets/javascripts/map/areas/south_eucalyptus_four.js
@@ -1,33 +1,29 @@
-var southEucalyptusFourPolygon;
-function addSouthEucalyptusFourPolygon(){
-  var southEucalyptusFourPolygonCoords = [
-{lat: -15.868728691582596, lng: -47.84151077270508},
-{lat: -15.868656451797039, lng: -47.84162878990173},
-{lat: -15.868718371614813, lng: -47.84169316291809},
-{lat: -15.868852531154765, lng: -47.84180045127869},
-{lat: -15.868997010559449, lng: -47.84190773963928},
-{lat: -15.869100210070838, lng: -47.84175753593445},
-{lat: -15.869285969058147, lng: -47.84167170524597},
-{lat: -15.869420128220272, lng: -47.84148931503296},
-{lat: -15.86945108801424, lng: -47.84128546714783},
-{lat: -15.869327248809848, lng: -47.84116744995117},
-{lat: -15.86920340952935, lng: -47.84116744995117},
-{lat: -15.869100210070838, lng: -47.841113805770874},
-{lat: -15.86893509082727, lng: -47.84100651741028},
-{lat: -15.868873171076059, lng: -47.841103076934814},
-{lat: -15.868862851115678, lng: -47.84122109413147},
-{lat: -15.868739011549847, lng: -47.84141421318054},
-  ];
+var southEucalyptusFour;
 
-  // Construct the polygon.
-  southEucalyptusFourPolygon = new google.maps.Polygon({
-    paths: southEucalyptusFourPolygonCoords,
-    strokeColor: '#A1BF77',
-    strokeOpacity: 0.1,
-    strokeWeight: 3,
-    fillColor: '#A1BF77',
-    fillOpacity: 0.50
-  });
+var southEucalyptusFourCoordinates = [
+  {lat: -15.868728691582596, lng: -47.84151077270508},
+  {lat: -15.868656451797039, lng: -47.84162878990173},
+  {lat: -15.868718371614813, lng: -47.84169316291809},
+  {lat: -15.868852531154765, lng: -47.84180045127869},
+  {lat: -15.868997010559449, lng: -47.84190773963928},
+  {lat: -15.869100210070838, lng: -47.84175753593445},
+  {lat: -15.869285969058147, lng: -47.84167170524597},
+  {lat: -15.869420128220272, lng: -47.84148931503296},
+  {lat: -15.86945108801424, lng: -47.84128546714783},
+  {lat: -15.869327248809848, lng: -47.84116744995117},
+  {lat: -15.86920340952935, lng: -47.84116744995117},
+  {lat: -15.869100210070838, lng: -47.841113805770874},
+  {lat: -15.86893509082727, lng: -47.84100651741028},
+  {lat: -15.868873171076059, lng: -47.841103076934814},
+  {lat: -15.868862851115678, lng: -47.84122109413147},
+  {lat: -15.868739011549847, lng: -47.84141421318054},
+];
+
+var southEucalyptusFourColor = '#A1BF77';
+
+function addSouthEucalyptusFour(){
+  southEucalyptusFour = createVegetation(southEucalyptusFourCoordinates,
+                                        southEucalyptusFourColor);
   
-  southEucalyptusFourPolygon.setMap(map);
+  southEucalyptusFour.setMap(map);
 }
