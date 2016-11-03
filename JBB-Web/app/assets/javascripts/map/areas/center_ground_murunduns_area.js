0ff2eddf8a6d98ae1ee774fd6265277f50686066
marimendes <mare.s.mendes@gmail.com>
2016-11-03 01:05:50 -0200

    Refactoring creation of center ground murunduns area Breaking vegetation creating function into two functions
    Signed-off-by: Mariana Mendes <mare.s.mendes@gmail.com>
    Signed-off-by: João Egewarth <egewarth@gmail.com>

--- JBB-Web/app/assets/javascripts/map/areas/center_ground_murunduns_area.js
+++ JBB-Web/app/assets/javascripts/map/areas/center_ground_murunduns_area.js
@@ -1,33 +1,29 @@
-var centerGroundMurundunsAreaPolygon;
-function addCenterGroundMurundunsAreaPolygon(){
-  var centerGroundMurundunsAreaPolygonCoords = [
-  {lat: -15.909927520359934, lng: -47.84158460802246},
-  {lat: -15.910216420733473, lng: -47.84162752328484},
-  {lat: -15.910835490214742, lng: -47.84162752328484},
-  {lat: -15.91126883771713, lng: -47.841069623809744},
-  {lat: -15.911557735532991, lng: -47.840275689941336},
-  {lat: -15.91139265111763, lng: -47.83900968728631},
-  {lat: -15.911000575087732, lng: -47.83847324548333},
-  {lat: -15.910567227006956, lng: -47.83797971902459},
-  {lat: -15.91036087044964, lng: -47.837593480926444},
-  {lat: -15.910154513680473, lng: -47.83785097299187},
-  {lat: -15.910154513680473, lng: -47.83847324548333},
-  {lat: -15.910319599112757, lng: -47.839052602630545},
-  {lat: -15.910484684409461, lng: -47.83943884072869},
-  {lat: -15.910484684409461, lng: -47.840039655548026},
-  {lat: -15.910195785051258, lng: -47.840340062957694},
-  {lat: -15.910030699517273, lng: -47.8401254862365}
-  ];
+var centerGroundMurundunsArea;
 
-    // Construct the polygon.
-  centerGroundMurundunsAreaPolygon = new google.maps.Polygon({
-    paths: centerGroundMurundunsAreaPolygonCoords,
-    strokeColor: '#42CCD0',
-    strokeOpacity: 0.1,
-    strokeWeight: 3,
-    fillColor: '#42CCD0',
-    fillOpacity: 0.50
-  });
+var centerGroundMurundunsAreaCoordinates = [
+    {lat: -15.909927520359934, lng: -47.84158460802246},
+    {lat: -15.910216420733473, lng: -47.84162752328484},
+    {lat: -15.910835490214742, lng: -47.84162752328484},
+    {lat: -15.91126883771713, lng: -47.841069623809744},
+    {lat: -15.911557735532991, lng: -47.840275689941336},
+    {lat: -15.91139265111763, lng: -47.83900968728631},
+    {lat: -15.911000575087732, lng: -47.83847324548333},
+    {lat: -15.910567227006956, lng: -47.83797971902459},
+    {lat: -15.91036087044964, lng: -47.837593480926444},
+    {lat: -15.910154513680473, lng: -47.83785097299187},
+    {lat: -15.910154513680473, lng: -47.83847324548333},
+    {lat: -15.910319599112757, lng: -47.839052602630545},
+    {lat: -15.910484684409461, lng: -47.83943884072869},
+    {lat: -15.910484684409461, lng: -47.840039655548026},
+    {lat: -15.910195785051258, lng: -47.840340062957694},
+    {lat: -15.910030699517273, lng: -47.8401254862365}
+];
 
-  centerGroundMurundunsAreaPolygon.setMap(map);
-} 
+var centerGroundMurundunsAreaColor = '#42CCD0';
+
+function addCenterGroundMurundunsArea(){
+    centerGroundMurundunsArea = createVegetation(centerGroundMurundunsAreaCoordinates,
+                                                 centerGroundMurundunsAreaColor);
+
+    centerGroundMurundunsArea.setMap(map);
+}
