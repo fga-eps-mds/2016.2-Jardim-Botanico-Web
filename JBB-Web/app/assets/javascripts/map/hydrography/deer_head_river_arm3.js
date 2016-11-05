7228a6c9baeb3c67760eaa530fdf6cabc7bd412c
marimendes <mare.s.mendes@gmail.com>
2016-11-05 12:26:20 +0000

    Refactoring creation of Deed Head River arm Decreasing river creating function and creating generic function
    Signed-off-by: Mariana Mendes <mare.s.mendes@gmail.com>
    Signed-off-by: João Egewarth <egewarth@gmail.com>

--- JBB-Web/app/assets/javascripts/map/hydrography/deer_head_river_arm3.js
+++ JBB-Web/app/assets/javascripts/map/hydrography/deer_head_river_arm3.js
@@ -1,40 +1,23 @@
 var deerHeadRiverArm3;
 
-function addDeerHeadRiverArm3(){
-    //set the path of the river
-    var deerHeadRiverArm3Coordinates = [
-        {lat: -15.871499583813398, lng: -47.85478393547237},
-        {lat: -15.87164406132045, lng: -47.854901952669024},
-        {lat: -15.871773059007067, lng: -47.85503069870174},
-        {lat: -15.8718246580586, lng: -47.85528282634914},
-        {lat: -15.8718246580586, lng: -47.85548667423427},
-        {lat: -15.871845297675515, lng: -47.855663700029254},
-        {lat: -15.87191237641587, lng: -47.85582999698818},
-        {lat: -15.87195365562961, lng: -47.85596947185695},
-        {lat: -15.87195365562961, lng: -47.85617868416011},
-        {lat: -15.871917536318046, lng: -47.8562484215945},
-        {lat: -15.871865937290305, lng: -47.856377167627215},
-        {lat: -15.871907216513552, lng: -47.85654346458614},
-        {lat: -15.871963975431727, lng: -47.85670976154506},
-        {lat: -15.871948495728363, lng: -47.856918973848224}
-    ];
+var deerHeadRiverArm3Coordinates = [
+    {lat: -15.871499583813398, lng: -47.85478393547237},
+    {lat: -15.87164406132045, lng: -47.854901952669024},
+    {lat: -15.871773059007067, lng: -47.85503069870174},
+    {lat: -15.8718246580586, lng: -47.85528282634914},
+    {lat: -15.8718246580586, lng: -47.85548667423427},
+    {lat: -15.871845297675515, lng: -47.855663700029254},
+    {lat: -15.87191237641587, lng: -47.85582999698818},
+    {lat: -15.87195365562961, lng: -47.85596947185695},
+    {lat: -15.87195365562961, lng: -47.85617868416011},
+    {lat: -15.871917536318046, lng: -47.8562484215945},
+    {lat: -15.871865937290305, lng: -47.856377167627215},
+    {lat: -15.871907216513552, lng: -47.85654346458614},
+    {lat: -15.871963975431727, lng: -47.85670976154506},
+    {lat: -15.871948495728363, lng: -47.856918973848224}
+];
 
-    //add the river at the map in the position previously defined
-    deerHeadRiverArm3 = new google.maps.Polyline({
-        path: deerHeadRiverArm3Coordinates,
-        map: map,
-        strokeColor: '#366ED4',
-        strokeWidth: 0.3,
-    });
-
-    //set the river on map
-    deerHeadRiverArm3.setMap(map);
-
-    google.maps.event.addListener(deerHeadRiverArm3, 'click', addDeerHeadRiverArm3Infowindow);
-}
-
-function addDeerHeadRiverArm3Infowindow(event){
-    var deerHeadRiverArm3ContentString =
+var deerHeadRiverArm3ContentString =
     '<div id="content">'+
     '<div id="siteNotice">'+
     '</div>'+
@@ -48,8 +31,15 @@ function addDeerHeadRiverArm3Infowindow(event){
     '</div>'+
     '</div>';
 
-    infowindow.close();
-    infowindow.setContent(deerHeadRiverArm3ContentString);
-    infowindow.setPosition(event.latLng);
-    infowindow.open(map);
+function addDeerHeadRiverArm3(){
+    //create the river
+    deerHeadRiverArm3 = createRiver(deerHeadRiverArm3Coordinates);
+
+    //set the river on map
+    deerHeadRiverArm3.setMap(map);
+
+    //add a listener to open a infowindow every time the river is clicked
+    google.maps.event.addListener(deerHeadRiverArm3, 'click', function(clicked_location) {
+        addInfowindow(clicked_location.latLng, deerHeadRiverArm3ContentString);
+    });
 }
