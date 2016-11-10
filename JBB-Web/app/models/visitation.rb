710ad8f59c7c6afcb137eec4ea0f48287fdfd517
Diego França <diegobmf@gmail.com>
2016-11-10 19:24:04 -0200

    Refactor of Visitation's model
    Signed-off-by: Diego França <diegobmf@gmail.com>
    Signed-off-by: Mariana Mendes <imare.s.mendes@gmail.com>

--- JBB-Web/app/models/visitation.rb
+++ JBB-Web/app/models/visitation.rb
@@ -2,41 +2,40 @@ class Visitation < ApplicationRecord
   belongs_to :user
 
 
-  def start_time
-    self.date
-  end
-
 	# validation of date
 	validates :date, presence: true
-
   # validation of time
   validates :time, presence: true
-
   #validation of visitants_paying
   validates :visitants_paying, presence: true
-
   # validation of description
   validates :description, presence: true, length: { minimum: 5, maximum: 300 }
-
-  #validation of visitants_amout
+  #validation of visitants_amount
   validates :visitants_amount, presence: true
+  validate :validate_visitants_amount
+
 
   def validate_visitants_amount
     if (self.visitants_amount > 100)
       errors.add(:visitants_amount, "A quantidade máxima por visitação é de 100 pessoas")
     elsif (self.visitants_amount < 1)
       errors.add(:visitants_amount, "A quantidade de pessoas não pode ser negativa")
+    elsif (self.visitants_amount < self.visitants_paying)
+      errors.add(:visitants_amount, "A quantidade de pagantes não pode ser maior que o total de visitantes")
     end
   end
+
 
   #status
   def set_status_default
     self.status = "Aguardando confirmacao"
   end
 
+
   def self.total
     self.sum(:visitation_cost)
   end
+
 
   def set_visitation_cost
     if (self.visitation_type <=> "Escola publica")
@@ -46,17 +45,21 @@ class Visitation < ApplicationRecord
     end
   end
 
+
   def canceled_by_employee
     self.status = "Cancelado por funcionario"
   end
+
 
   def canceled_by_user
     self.status = "Cancelado pelo usuario"
   end
 
+
   def refused_by_employee
     self.status = "Recusado por funcionario"
   end
+
 
   def accepted_by_employee
     self.status = "Agendado"
