6ec0b7d6f949f4815485fbc0da7633d51efcf00c
Diego França <diegobmf@gmail.com>
2016-11-10 21:07:20 -0200

    Fix error in Trail's migrate
    Signed-off-by: Diego França <diegobmf@gmail.com>

--- JBB-Web/db/schema.rb
+++ JBB-Web/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161110221428) do
+ActiveRecord::Schema.define(version: 20161008181353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.integer  "user_id"
     t.date     "date_start"
@@ -47,6 +47,15 @@ ActiveRecord::Schema.define(version: 20161110221428) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_phones_on_user_id", using: :btree
+  end
+
+  create_table "trails", force: :cascade do |t|
+    t.string   "trail_name"
+    t.float    "length"
+    t.string   "locomotion"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -89,5 +98,4 @@ ActiveRecord::Schema.define(version: 20161110221428) do
     t.index ["status"], name: "index_visitations_on_status", using: :btree
     t.index ["user_id"], name: "index_visitations_on_user_id", using: :btree
   end
-
 end
