a83cb7fe214fa69363a62bea9b81ccccf90f73ef
Gustavo Moreira <gustavomn93@gmail.com>
2016-11-11 01:04:46 +0000

    Fixing accepting tests Fixing the authentication steps for users

--- JBB-Web/features/step_definitions/user_login_steps.rb
+++ JBB-Web/features/step_definitions/user_login_steps.rb
@@ -3,17 +3,39 @@ Given(/^I have accessed the login page$/) do
 end
 
 Given(/^I already have an account$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @user = User.new
+  @user.name = 'Nome Sobrenome'
+  @user.email = 'gustavo@gmail.com'
+  @user.password = '123456'
+  @user.cpf = '02954492171'
+  @user.gender = 'Masculino'
+  @user.birth = '01011991'
+  @user.save
 end
 
 When(/^I try to login with a wrong email$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^I am redirected to the login page$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @user = User.new
+  @user.name = 'Nome Sobrenome'
+  @user.email = 'gustavo'
+  @user.password = '123456'
+  @user.cpf = '02954492171'
+  @user.gender = 'Masculino'
+  @user.birth = '01011991'
+  @user.save
 end
 
 When(/^I try to login with a wrong password$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @user = User.new
+  @user.name = 'Nome Sobrenome'
+  @user.email = 'gustavo@gmail.com'
+  @user.password = '123'
+  @user.cpf = '02954492171'
+  @user.gender = 'Masculino'
+  @user.birth = '01011991'
+  @user.save
+end
+
+
+Then(/^I am redirected to the login page$/) do
+  visit '/sign_in'
 end
