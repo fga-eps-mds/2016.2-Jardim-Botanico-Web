afa284e9ce1156e1ac5fb6dacc44f74196705905
Gustavo Moreira <gustavomn93@gmail.com>
2016-11-11 01:04:46 +0000

    Fixing accepting tests Fixing the authentication steps for users

--- JBB-Web/features/step_definitions/user_login_steps.rb
+++ JBB-Web/features/step_definitions/user_login_steps.rb
@@ -40,3 +40,19 @@ end
 Then(/^I get the error message$/) do
   page.should have_content ('Email ou senha inválidos')
 end
+
+When(/^I try to login with a wrong password$/) do
+  @user = User.new
+  @user.name = 'Nome Sobrenome'
+  @user.email = 'gustavo@gmail.com'
+  @user.password = '123'
+  @user.cpf = '02954492171'
+  @user.gender = 'Masculino'
+  @user.birth = '01011991'
+  @user.save
+end
+
+
+Then(/^I am redirected to the login page$/) do
+  visit '/sign_in'
+end
