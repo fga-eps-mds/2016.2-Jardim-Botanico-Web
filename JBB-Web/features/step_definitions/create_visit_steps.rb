c058ba44989d733bc1d69b1a120dc0f41a70dd14
Adailson Santos <adailson2@gmail.com>
2016-11-12 17:30:56 +0000

    Acceptance Tests
    Adding feature and step for create visit

---
+++ JBB-Web/features/step_definitions/create_visit_steps.rb
@@ -0,0 +1,23 @@
+Given(/^I am on the create visit page$/) do
+  visit('/visitations/new')
+end
+
+When(/^I fill the new visit form$/) do
+   
+   select('2016', from: 'visitation_date_1i')
+   select('November', from: 'visitation_date_2i')
+   select('11', from: 'visitation_date_3i')
+   select('Educação', from: 'visitation_objective')
+   select('Trilhas', from: 'visitation_spaces')
+   fill_in 'visitation_visitants_amount', :with => "30"
+   fill_in 'visitation_groups_age', :with => "15"
+   fill_in 'visitation_visitants_paying', :with => "5"
+   choose('visitation_visitation_type_pessoa_fisica')
+   choose('visitation_has_guide_true')
+   fill_in 'visitation_description', :with => "Teste descrição"
+  
+end
+
+When(/^I click in 'Cadastrar'$/) do
+   click_button('registration')
+end
