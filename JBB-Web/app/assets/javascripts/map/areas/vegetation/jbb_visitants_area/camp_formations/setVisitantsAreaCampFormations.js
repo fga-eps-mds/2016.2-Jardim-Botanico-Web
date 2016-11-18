0af8a23307775e983386a510a5616faee3b09fae
Mariana Mendes <mare.s.mendes@gmail.com>
2016-11-18 23:14:51 +0000

    Add a new area on vegetation's map Add the Small clean camp area to vegetation's map

--- JBB-Web/app/assets/javascripts/map/areas/vegetation/jbb_visitants_area/camp_formations/setVisitantsAreaCampFormations.js
+++ JBB-Web/app/assets/javascripts/map/areas/vegetation/jbb_visitants_area/camp_formations/setVisitantsAreaCampFormations.js
@@ -1,14 +1,17 @@
 function removeVegetationVisitantsAreaCampFormations(){
     //remove camp formations
     cleanCamp.setMap(null);
+    smallCleanCamp.setMap(null);
     southDirtyFieldArea.setMap(null);
     southDirtyField.setMap(null);
     typicalCerradoCleanCamp.setMap(null);
+
 }
 
 function addVegetationVisitantsAreaCampFormations(){
     //add camp formations
     addCleanCamp();
+    addSmallCleanCamp();
     addSouthDirtyFieldArea();
     addSouthDirtyField();
     addTypicalCerradoCleanCamp();
