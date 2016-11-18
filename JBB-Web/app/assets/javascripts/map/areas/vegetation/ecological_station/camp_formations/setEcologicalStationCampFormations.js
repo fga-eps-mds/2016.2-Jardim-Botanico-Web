c932157c794da3142890ecc75d4d31f5c8b3154c
João Henrique Egewarth <egewarth@gmail.com>
2016-11-18 21:34:32 +0000

    Refactoring creation of vegetations in ecological station Breaking creation function of vegetation in three functions
    Signed-off-by: João Henrique Egewarth <egewarth@gmail.com>

---
+++ JBB-Web/app/assets/javascripts/map/areas/vegetation/ecological_station/camp_formations/setEcologicalStationCampFormations.js
@@ -0,0 +1,11 @@
+function removeVegetationEcologicalStationCampFormations(){
+    //remove camp formations
+    dirtyFieldTypicalCerrado.setMap(null);
+    southCleanCamp.setMap(null);
+}
+
+function addVegetationEcologicalStationCampFormations(){
+    //add camp formations
+    addDirtyFieldTypicalCerrado();
+    addSouthCleanCamp();
+}
--- JBB-Web/app/assets/javascripts/map/areas/vegetation/ecological_station/setVegetationEcologicalStation.js
+++ JBB-Web/app/assets/javascripts/map/areas/vegetation/ecological_station/setVegetationEcologicalStation.js
@@ -1,36 +1,32 @@
 function removeVegetationEcologicalStation(){
     removeVegetationEcologicalStationCerradoSensuStrictu();
 
-    centerBypassAreaCenter.setMap(null);
-    centerBypassAreaLeft.setMap(null);
-    centerBypassAreaRight.setMap(null);
-    centerBypassAreaUp.setMap(null);
+    removeVegetationEcologicalStationCampFormations();
+
+    removeVegetationEcologicalStationPaths();
+
     centerForestGalleryArea.setMap(null);
+
     centerGroundAreaRight.setMap(null);
     centerGroundMurundunsAreaCenter.setMap(null);
     centerGroundMurundunsAreaLeft.setMap(null);
     centerGroundMurundunsAreaRight.setMap(null);
     centerGroundMurundunsArea.setMap(null);
+
     centerThickArea.setMap(null);
-    dirtyFieldTypicalCerrado.setMap(null);
     smallRockCerrado.setMap(null);
     southAnthropicArea.setMap(null);
-    southBypassArea.setMap(null);
-    southCenterBypassArea.setMap(null);
-    southCleanCamp.setMap(null);
     southGalleryBush.setMap(null);
-    southLeftBypassArea.setMap(null);
-    southPath.setMap(null);
     southRupestresAreaTop.setMap(null);
 }
 
 function addVegetationEcologicalStation(){
     addVegetationEcologicalStationCerradoSensuStrictu();
 
-    addCenterBypassAreaCenter();
-    addCenterBypassAreaLeft();
-    addCenterBypassAreaRight();
-    addCenterBypassAreaUp();
+    addVegetationEcologicalStationCampFormations();
+
+    addVegetationEcologicalStationPaths();
+
     addCenterForestGalleryArea();
     addCenterGroundAreaRight();
     addCenterGroundMurundunsAreaCenter();
@@ -38,14 +34,9 @@ function addVegetationEcologicalStation(){
     addCenterGroundMurundunsAreaRight();
     addCenterGroundMurundunsArea();
     addCenterThickArea(); //CERRADÃO
-    addDirtyFieldTypicalCerrado();
+
     addSmallRockCerrado();
     addSouthAnthropicArea();
-    addSouthBypassArea();
-    addSouthCenterBypassArea();
-    addSouthCleanCamp();
     addSouthGalleryBush();
-    addSouthLeftBypassArea();
-    addSouthPath();
     addSouthRupestresAreaTop();
 }
