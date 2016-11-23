e584886d98a6dfcaadb72dd8cc640de7a146f2f4
Mariana Mendes <mare.s.mendes@gmail.com>
2016-11-23 23:43:41 +0000

    Correct the user mailer Fixing some issues on user mailer(user, event and visitation)

--- JBB-Web/app/mailers/user_mailer.rb
+++ JBB-Web/app/mailers/user_mailer.rb
@@ -4,20 +4,20 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user
     @url  = 'http://localhost:3000/sign_in'
-    mail(content_type: 'mulipart/alternative', to: @user.email, subject: "Bem Vindo ao Jardim Botânico de Brasília")
+   mail(to: @user.email, subject: "Bem Vindo ao Jardim Botânico de Brasília")
   end
 
   def change_status_visitation(visitation)
     @user = User.find(visitation.user_id)
     @visitation = visitation
     @url  = 'http://localhost:3000/visitations/show'
-  	mail(content_type: 'mulipart/alternative', to: @user.email, subject: "Seu status de agendamento foi alterado")
+  	mail(to: @user.email, subject: "Seu status de visita foi alterado")
   end
 
   def change_status_event(event)
     @user = User.find(event.user_id)
     @event = event
     @url  = 'http://localhost:3000/events/show_user'
-    mail(content_type: 'mulipart/alternative', to: @user.email, subject: "Seu status de agendamento foi alterado")
+    mail(to: @user.email, subject: "Seu status de evento foi alterado")
   end
-end
+end 
